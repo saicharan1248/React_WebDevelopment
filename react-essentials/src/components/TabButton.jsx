0b09e987aaf2f7c45fe79dbaf4fb7f05b3198355
Saicharan Pagidimunthala <saicharan8999@gmail.com>
2024-04-22 19:53:27 -0500

    Initial commit

---
+++ react-essentials/src/components/TabButton.jsx
@@ -0,0 +1,9 @@
+export default function TabButton({ children, onSelect, isSelected }) {
+  return (
+    <li>
+      <button className={isSelected ? "active" : null} onClick={onSelect}>
+        {children}
+      </button>
+    </li>
+  );
+}
